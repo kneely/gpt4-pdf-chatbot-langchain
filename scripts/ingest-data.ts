bfab5f79c6a4f3d72dd40b0dce0529234c394b46
Kevin Neely <kevin.r.neely@outlook.com>
2023-06-13 08:48:23 -0400

    checkpoint

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -1,66 +1,42 @@
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { DirectoryLoader, UnknownHandling } from 'langchain/document_loaders/fs/directory';
+import { CustomPDFLoader } from '@/utils/customPDFLoader';
+import { UnstructuredDirectoryLoader } from "langchain/document_loaders/fs/unstructured";
+import { CheerioWebBaseLoader, PDFLoader, UnstructuredLoader } from 'langchain/document_loaders';
+import axios from 'axios';
+import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
+import { pinecone } from '@/utils/pinecone-client';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
-import { pinecone } from '@/utils/pinecone-client';
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
-import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
-import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
-import {
-  PuppeteerEvaluate,
-  PuppeteerWebBaseLoader,
-} from 'langchain/document_loaders';
-import { getRMAHandbooks } from './rma-handbook-loader';
-import { Document } from 'langchain/document';
-import { Browser, Page, Puppeteer } from 'puppeteer';
-import { PuppeteerWebBaseLoaderOptions } from 'langchain/dist/document_loaders/web/puppeteer';
 
 /* Name of directory to retrieve your files from 
    Make sure to add your PDF files inside the 'docs' folder
 */
-const filePath = 'docs';
-
-/*load raw docs from the all files in the directory */
-// const directoryLoader = new DirectoryLoader(filePath, {
-//       '.pdf': (path) => new PDFLoader(path),
-// });
-
-// const rawDocs = await directoryLoader.load();
-
-// const webOptions: PuppeteerWebBaseLoaderOptions = {
-//   evaluate: 'page'
-// }
+const localPath = 'docs/queue';
+const pdfUrl = 'https://www.rma.usda.gov/-/media/RMA/Handbooks/Privately-Developed-Products---20000/Florida-Citrus-Fruit/2023-20650L-1H-APH-Florida-Citrus-Fruit-Loss-Adjustment-Standards-Handbook.ashx';
 
 export const run = async () => {
   try {
-    let rawDocs: Document<Record<string, any>>[] = [];
-    const urls = await getRMAHandbooks();
+    /*load raw docs from the all files in the directory */
+    const directoryLoader = new DirectoryLoader(localPath, {
+      '.pdf': (path) => new CustomPDFLoader(path),
+    });
 
-    await Promise.any(
-      urls.map(async (item) => {
-        const webLoader = new PuppeteerWebBaseLoader(item, {
-          launchOptions: {
-            headless: true,
-          },
-          gotoOptions: {
-            waitUntil: 'domcontentloaded',
-          },
-          /** Pass custom evaluate, in this case you get page and browser instances */
-          async evaluate(page: Page, browser: Browser) {
-            const result = await page.evaluate(() => {
-              return new PDFLoader(page.);
-            });
-            return result;
-          },
-        });
-        let res = await webLoader.load();
-        console.log(res);
-        rawDocs.push(...res);
-      }),
-    );
+    const loader = new UnstructuredDirectoryLoader(localPath, {
+      apiKey: "test",
+      apiUrl: "http://localhost:8000/general/v0/general",
+      recursive: true,
+      unknown: UnknownHandling.Ignore,
+
+    })
+
+    const rawDocs = await loader.load();
+
+    console.log(rawDocs);
 
     /* Split text into chunks */
     const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
+      chunkSize: 4000,
       chunkOverlap: 200,
     });
 
--- scripts/rma-handbook-loader.ts
+++ scripts/rma-handbook-loader.ts
@@ -1,62 +1,68 @@
-import { link } from 'fs';
 import puppeteer, { Browser, Page } from 'puppeteer';
+import * as fs from 'fs';
 
-async function getPdfLinks(url: string): Promise<string[]> {
+
+type RMALink = {
+    "name": string,
+    "link": string
+}
+
+async function getPdfLinks(url: string): Promise<RMALink[]> {
     const browser: Browser = await puppeteer.launch();
     const page: Page = await browser.newPage();
     await page.goto(url);
+    await page.waitForNetworkIdle();
 
-    // Extract PDF links
-    const allLinks = await page.evaluate(() =>
-        Array.from(document.querySelectorAll('a[href$=".ashx"]')).map(
-            (link) => (link as HTMLAnchorElement).href,
-        ),
+    const dataTable = await page.$('#handbookDataTable');
+    const rows = await dataTable?.$$('tbody tr');
+    const links = await Promise.all(
+        rows?.map(async (row) => {
+            const firstCell = await row.$(`td:nth-child(1)`);
+            const link = await firstCell?.$('a');
+            return link ? { name: await link.evaluate((node) => node.textContent?.trim()), href: "https://www.rma.usda.gov" + await link.evaluate((node) => node.getAttribute('href')) } : null;
+        }) ?? [],
     );
 
-    const regex = /media\/RMA\/Handbooks/;
+    const nextButton = await page.$('#handbookDataTable_next');
+    let isDisabled = await nextButton!.evaluate(button => button.classList.contains('disabled'));
+    while (!isDisabled) {
+        const nextButton = await page.$('#handbookDataTable_next');
+        isDisabled = await nextButton!.evaluate(button => button.classList.contains('disabled'));
+        if (!isDisabled) {
+            await nextButton?.click();
+            await page.waitForNetworkIdle();
+            const nextRows = await dataTable?.$$('tbody tr');
+            const nextLinks = await Promise.all(
+                nextRows?.map(async (row) => {
+                    const firstCell = await row.$(`td:nth-child(1)`);
+                    const link = await firstCell?.$('a');
+                    return link ? {
+                        name: await link.evaluate((node) => node.textContent?.trim()), href: "https://www.rma.usda.gov" + await link.evaluate((node) => node.getAttribute('href'))
+                    } : null;
+                }) ?? [],
+            );
+            links.push(...nextLinks);
+        }
+    }
 
-    const pdfLinks = allLinks.filter((item) => item.match(regex));
+    // const regex = /media\/RMA\/Handbooks/;
+    // const regex2 = /ftp\/Publications\/M13_Handbook/;
+
+    // const pdfLinks = allLinks.filter((item) => item.link.match(regex));
+    const pdfLinks: RMALink[] = links.map((item) => {
+        let x: RMALink = {
+            name: item?.name || "",
+            link: item?.href || ""
+        }
+        return x;
+    })
 
     await browser.close();
 
     return pdfLinks;
 }
 
-async function getPdfBlob(url: string) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    // Delay between requests
-    await new Promise(resolve => setTimeout(resolve, 1000)); // 1 second delay
-
-    let response;
-    try {
-        // Navigating to the page and treating the response as ArrayBuffer
-        response = await page.goto(url, { waitUntil: 'networkidle0', timeout: 120000 }); // timeout after 120 seconds
-    } catch (error: Error | any) {
-        console.error(`Error navigating to URL: ${url}`);
-        console.error(`Error message: ${error.message}`);
-        await browser.close();
-        return null;
-    }
-
-    if (!response) {
-        console.error('No response from the server.');
-        await browser.close();
-        return null;
-    }
-
-    // Get the pdf as a Buffer
-    const pdfBuffer = await response.buffer();
-
-    await browser.close();
-
-    // Use the buffer as needed
-    // Here we're just returning it
-    return pdfBuffer;
-}
-
-export const getRMAHandbooks = async (): Promise<string[]> => {
+export const getRMAHandbooks = async (): Promise<RMALink[]> => {
     const urls: string[] = [
         'https://www.rma.usda.gov/Policy-and-Procedure/Program-Administration---14000',
         'https://www.rma.usda.gov/Policy-and-Procedure/Coverage-Plans---18000',
@@ -66,8 +72,7 @@ export const getRMAHandbooks = async (): Promise<string[]> => {
         'https://www.rma.usda.gov/Policy-and-Procedure/Loss-Adjustment-Standards---25000',
     ];
 
-    let links: string[] = [];
-    let buffers: Buffer[] = [];
+    let links: RMALink[] = [];
 
     for (const url of urls) {
         try {
@@ -82,27 +87,57 @@ export const getRMAHandbooks = async (): Promise<string[]> => {
         await new Promise(resolve => setTimeout(resolve, 1000)); // 1 second delay
     }
 
-    for (const url of links) {
-        try {
-            const res = await getPdfBlob(url);
-            if (res) {
-                buffers.push(res);
-            }
-        } catch (error) {
-            console.error(`Failed to get PDF blob for URL: ${url}`);
-            console.error(`Error: ${error}`);
-        }
-
-        // Add a delay between requests
-        await new Promise(resolve => setTimeout(resolve, 1000)); // 1 second delay
-    }
-
     return links;
 };
 
+async function testPage() {
+    const browser: Browser = await puppeteer.launch();
+    const page: Page = await browser.newPage();
+    await page.goto('https://www.rma.usda.gov/Policy-and-Procedure/Underwriting---24000');
+
+    await page.waitForNetworkIdle();
+
+    const dataTable = await page.$('#handbookDataTable');
+    const rows = await dataTable?.$$('tbody tr');
+    const links = await Promise.all(
+        rows?.map(async (row) => {
+            const firstCell = await row.$(`td:nth-child(1)`);
+            const link = await firstCell?.$('a');
+            return link ? { name: await link.evaluate((node) => node.textContent?.trim()), href: await link.evaluate((node) => node.getAttribute('href')) } : null;
+        }) ?? [],
+    );
+
+    const nextButton = await page.$('#handbookDataTable_next');
+    let isDisabled = await nextButton!.evaluate(button => button.classList.contains('disabled'));
+    while (!isDisabled) {
+        const nextButton = await page.$('#handbookDataTable_next');
+        isDisabled = await nextButton!.evaluate(button => button.classList.contains('disabled'));
+        if (!isDisabled) {
+            await nextButton?.click();
+            await page.waitForNetworkIdle();
+            const nextRows = await dataTable?.$$('tbody tr');
+            const nextLinks = await Promise.all(
+                nextRows?.map(async (row) => {
+                    const firstCell = await row.$(`td:nth-child(1)`);
+                    const link = await firstCell?.$('a');
+                    return link ? {
+                        name: await link.evaluate((node) => node.textContent?.trim()), href: "https://www.rma.usda.gov" + await link.evaluate((node) => node.getAttribute('href'))
+                    } : null;
+                }) ?? [],
+            );
+            links.push(...nextLinks);
+        }
+    }
+
+    console.log(links.filter((link) => link !== null));
+
+    await browser.close();
+}
 
 (async () => {
     const results = await getRMAHandbooks();
-    console.log(results);
+    fs.writeFileSync("docs/links.json", JSON.stringify(results))
+    console.log(`Found ${results.length} files`);
+    // await testPage();
     console.log('ingestion complete');
 })();
